be6d0f4ac68072ba0f3fc0192f600fccac5b408c
MRKORI97 <114434820+MRKORI97@users.noreply.github.com>
2022-09-27 11:27:34 +0530

    Java program language

---
+++ Problem_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem_2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of a");
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+		System.out.println((2*i-1)+" ");
+		}
+	}
+}
